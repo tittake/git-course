1270322b8ddc5fb484e09b53b886730fbcd13afa
tittake <titta.kemppi@tuni.fi>
2020-09-06 14:38:24 +0300

    Add another test code to work on git

---
+++ teht7.5 Tanssipelien pisteet.py
@@ -0,0 +1,9 @@
+# TIE-02100 Johdatus ohjelmointiin
+# Tehtävä: dict_dokumentaatio, ohjelmakoodipohja
+
+
+
+def calculate_average(song_result):
+
+    return (sum(song_result.values())/len(song_result))
+
